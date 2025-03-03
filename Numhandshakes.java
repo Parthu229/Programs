1d454e315bf36c0a26a41a43929dd3515da98d2e
Parthu229 <reddymanjunath849@gmail.com>
2025-03-03 15:23:42 +0530

    Numhandshakes.java

--- Numhandshakes.java
+++ Numhandshakes.java
@@ -2,9 +2,7 @@ import java.util.*;
 public class Numhandshakes {
 
 	public static void main(String[] args) {
-		/* 1   2  3  4    5    6 
-		 * 0   1  3  6    10   15 => 0 2 6 12 14
-		 */
+		
 		Scanner s = new Scanner(System.in);
 		int n = s.nextInt();
 		int ans  =0;
